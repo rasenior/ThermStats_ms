f206880fbd04d08c42d562fef9cd941f4b14e15c
Rebecca A. Senior <rebecca.a.senior@gmail.com>
2019-06-02 11:44:39 +0100

    Reducing replication to 50
    Signed-off-by: Rebecca A. Senior <rebecca.a.senior@gmail.com>

--- batch_extract_benchmark.R
+++ batch_extract_benchmark.R
@@ -1,5 +1,6 @@
 
 microbenchmark::microbenchmark(
     ThermStats::batch_extract(in_dir = "data/flirstack_pics",
-                              write_results = FALSE)
+                              write_results = FALSE), 
+    times = 50
 )
